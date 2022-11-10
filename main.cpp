015338318f5b8336f5f909f6a1a053a7f4557f2a
peytonjb04 <117470858+peytonjb04@users.noreply.github.com>
2022-11-10 16:13:04 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,78 +20,82 @@ int main() {
 
   while (num < 1000)
   {
-    if (num % 2 == 0)
-  {
-    num = prime;
-    cout << 2<< " ";
-    }
-    
-  if (num % 3 == 0)
-  {
-    num = prime;
-    cout << 3 << " ";
-    }
+    if (num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0 || num % 11 == 0 || num % 13 == 0 || num % 17 == 0 || num % 19 == 0 || num % 23 == 0 || num % 29 == 0 || num % 31 == 0){
+      if (num % 2 == 0)
+      {
+        num = prime;
+        cout << "Can be divided by 2 ";
+        }
+      if (num % 3 == 0)
+      {
+        num = prime;
+        cout << "Can be divided by 3 ";
+        }
         
-  if (num % 5 == 0)
-  {
-    num = prime;
-    cout << 5<< " ";
-    
-    }
+       if (num % 5 == 0)
+      {
+        num = prime;
+        cout << "Can be divided by 5 ";
+        }
         
-  if (num % 7 == 0)
-  {
-    num = prime;
-    cout << 7 << " ";
-    }
+       if (num % 7 == 0)
+      {
+        num = prime;
+        cout << "Can be divided by 7 ";
+        }
         
-  if (num % 11 == 0)
-  {
-    num = prime;
-    cout << 11 << " ";
-    }
-  if (num % 13 == 0)
-    {
-      num = prime;
-      cout << 13 << " ";
+       if (num % 11 == 0)
+      {
+        num = prime;
+        cout << "Can be divided by 11 ";
+        }
+       if (num % 13 == 0)
+      {
+        num = prime;
+        cout << "Can be divided by 13 ";
+        }
+        
+       if (num % 17 == 0)
+      {
+        num = prime;
+        cout << "Can be divided by 17 ";
+        }
+        
+       if (num % 19 == 0)
+      {
+        num = prime;
+        cout << "Can be divided by 19 ";
+        }
+        
+       if (num % 23 == 0)
+      {
+        num = prime;
+        cout << "Can be divided by 23 ";
+        }
+        
+       if (num % 29 == 0)
+      {
+       num = prime;
+       cout << "Can be divided by 29 ";
       }
-        
-  if (num % 17 == 0)
-  {
-    num = prime;
-    cout << 17 << " ";
-    }
-        
-  if (num % 19 == 0)
-  {
-    num = prime;
-    cout << 19 << " ";
-    }
-        
-  if (num % 23 == 0)
-  {
-    num = prime;
-    cout << 23 << " ";
-    }
-        
-  if (num % 29 == 0)
-  {
-    num = prime;
-    cout << 29 << " ";
-    }
-        
-  if (num % 31 == 0)
-  {
-    num = prime;
-    cout << 31 << " ";
+    
+      if (num % 31 == 0)
+      {
+       num = prime;
+       cout << "Can be divided by 31 ";
+        }
+
     }
 
-  else if (num != prime) {
-    cout << "Number is prime";
-  }
+    else {
+      cout << "The number is prime";
+      }
 
-    break;
-  
+      break;
+      
+    
+    
+
   }
   
   
